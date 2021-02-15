69f1d34fc4dc2b82012e1e137fb1c68c0784c69f
evelyn-sz <evelynszarek@gmail.com>
2021-02-15 20:23:20 +0000

    RollerCoaster charges based on height

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,20 @@
 package attractions;
+import behaviours.ITicketed;
+import people.Visitor;
 
-public class RollerCoaster  extends Attraction {
+public class RollerCoaster extends Attraction implements ITicketed {
 
+    private double price;
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+        this.price = 8.50;
+    }
+
+    public double defaultPrice(){
+        return this.price;
+    }
+
+    public double priceFor(Visitor visitor){
+        return visitor.getHeight() > 200 ? 2 * this.price : this.price;
     }
 }
--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,27 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor;
+    Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
+    Visitor visitor5;
 
     @Before
     public void setUp() {
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor =   new Visitor(18, 201, 10);
+        visitor2 =  new Visitor(18, 150, 10);
+        visitor3 =  new Visitor(18, 150, 10);
+        visitor4 =  new Visitor(11, 150, 10);
+        visitor5 =  new Visitor(11, 140, 10);
     }
 
     @Test
@@ -27,5 +38,16 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice(){
+        assertEquals(8.50, rollerCoaster.defaultPrice(), 0.0);
+    }
+
+    @Test
+    public void chargesBasedOnHeight(){
+        assertEquals(17, rollerCoaster.priceFor(visitor), 0.0);
+        assertEquals(8.50, rollerCoaster.priceFor(visitor2), 0.0);
     }
 }
