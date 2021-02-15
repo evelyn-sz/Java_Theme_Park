2b423952d4b021f4cc0f5b5c37c85e19546657f7
evelyn-sz <evelynszarek@gmail.com>
2021-02-15 20:47:56 +0000

    TobaccoStall checks age and implements ISecurity

--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,21 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor mediumAdult;
+    Visitor mediumTeen;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        mediumAdult =   new Visitor(18, 150, 10);
+        mediumTeen =    new Visitor(14, 150, 10);
     }
 
     @Test
@@ -27,5 +32,11 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void checkAge(){
+        assertEquals(true, tobaccoStall.isAllowedTo(mediumAdult));
+        assertEquals(false, tobaccoStall.isAllowedTo(mediumTeen));
     }
 }
